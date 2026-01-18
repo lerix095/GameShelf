161c4e1503329e91b5c43c4db4ed2df30c3555f8
Valeriy Protsenko <valgholz@gmail.com>
2026-01-18 17:23:49 +0200

    Initial Commit

---
+++ P12M3L8/P12M3L8App.swift
@@ -0,0 +1,17 @@
+//
+//  P12M3L8App.swift
+//  P12M3L8
+//
+//  Created by Valeriy Protsenko on 18.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct P12M3L8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
